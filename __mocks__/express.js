5bae5ee06cf166947453b1f748f2cd44e21b8094
Anna Scheuler <scheul93@gmail.com>
2018-06-06 09:19:12 -0500

    updated unit tests

--- __mocks__/express.js
+++ __mocks__/express.js
@@ -1,3 +1,6 @@
+let __routeMap = {};
+let __portInUse = '';
+
 function server() {
     return {
         use,
@@ -5,11 +8,17 @@ function server() {
     };
 }
 
-function use() {
+function use(route, val) {
+    if (__routeMap[route]) {
+        __routeMap[route].push(val);
+    } else {
+        __routeMap[route] = [val];
+    }
     return;
 }
 
 function listen(port, callback) {
+    __portInUse = port;
     callback();
 }
 
@@ -17,6 +26,21 @@ function staticFn(path) {
     return path;
 }
 
+function __getItemsForRoute(route) {
+    return __routeMap[route];
+}
+
+function __getPortInUse() {
+    return __portInUse;
+}
+
+function __resetServer() {
+    __routeMap = {};
+    __portInUse = '';
+}
 
 server.static = staticFn;
+server.__getItemsForRoute = __getItemsForRoute;
+server.__getPortInUse = __getPortInUse;
+server.__resetServer = __resetServer;
 module.exports = server;
--- index.test.js
+++ index.test.js
@@ -1,26 +1,25 @@
 const skeletorLocalServer = require('./index');
 
+let express;
 jest.mock('express');
-
-global.console = {
-    log: jest.fn()
-};
 
 const logger = {
     info: () => {},
     error: () => {}
 };
 const options = {logger};
-let consoleSpy;
+let logInfoSpy;
 
 describe('local server plugin', () => {
 
     beforeEach(() => {
-        consoleSpy = jest.spyOn(global.console, 'log');
+        express = require('express');
+        logInfoSpy = jest.spyOn(logger, 'info');
     });
 
     afterEach(() => {
-        consoleSpy.mockReset();
+        express.__resetServer();
+        logInfoSpy.mockReset();
     });
 
     it('should error when no entry point specified', async () => {
@@ -45,7 +44,7 @@ describe('local server plugin', () => {
         await expect(skeletorLocalServer().run(config, options)).rejects.toEqual(expectedError);
     });
 
-    it('should spin up a local server', () => {
+    it('should log when server starts', () => {
         const expectedResp = {
             status: 'running'
         };
@@ -56,9 +55,36 @@ describe('local server plugin', () => {
         };
 
         return skeletorLocalServer().run(config, options).then(resp => {
-            expect(consoleSpy).toHaveBeenCalledTimes(1);
-            expect(consoleSpy).toHaveBeenCalledWith(expectedMessage);
+            expect(logInfoSpy).toHaveBeenCalledTimes(1);
+            expect(logInfoSpy).toHaveBeenCalledWith(expectedMessage);
             expect(resp).toEqual(expectedResp);
+        });
+    });
+
+    it('should assign asset path to correct route', () => {
+        const expectedRouteVal = 'testDir/testDir';
+        const config = {
+            entry: 'testDir',
+            currentDirectory: 'testDir'
+        };
+
+        return skeletorLocalServer().run(config, options).then(() => {
+            const routeItems = express.__getItemsForRoute('/');
+            expect(routeItems).toHaveLength(1);
+            expect(routeItems[0]).toEqual(expectedRouteVal);
+        });
+    });
+
+    it('should default port to 3000', () => {
+        const expectedPort = 3000;
+        const config = {
+            entry: 'testDir',
+            currentDirectory: 'testDir'
+        };
+
+        return skeletorLocalServer().run(config, options).then(() => {
+            const currentPort = express.__getPortInUse();
+            expect(currentPort).toBe(expectedPort);
         });
     });
 
@@ -66,17 +92,21 @@ describe('local server plugin', () => {
         const expectedResp = {
             status: 'running'
         };
+        const expectedPort = 3001;
         const expectedMessage = 'Started server on port 3001';
         const config = {
-            port: 3001,
+            port: expectedPort,
             entry: 'testDir',
             currentDirectory: 'testDir'
         };
 
         return skeletorLocalServer().run(config, options).then(resp => {
-            expect(consoleSpy).toHaveBeenCalledTimes(1);
-            expect(consoleSpy).toHaveBeenCalledWith(expectedMessage);
+            expect(logInfoSpy).toHaveBeenCalledTimes(1);
+            expect(logInfoSpy).toHaveBeenCalledWith(expectedMessage);
             expect(resp).toEqual(expectedResp);
+
+            const currentPort = express.__getPortInUse();
+            expect(currentPort).toBe(expectedPort);
         });
     });
 
